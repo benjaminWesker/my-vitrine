537233e00ddf70c358997aea822a0d0c0b0110fa
benjaminWesker <49282299+benjaminWesker@users.noreply.github.com>
2021-11-05 12:21:50 +0100

    Revert "optimize burger btn"
    This reverts commit 87236ac50f0b3752b4b6c1b2156964ec1dc4866d.

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -8,6 +8,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class BannerComponent implements OnInit {
 
   menuMobileIsOpen = false;
+  /* on évite les appuis trop rapide sur le bouton */
+  freeBtnBurger = true;
+
   btnMenuMobileState = "";
 
   constructor() {}
@@ -16,26 +19,72 @@ export class BannerComponent implements OnInit {
 
   }
 
+  /**
+   * A l'aapui du bouton menu burger en fonction de son état =>
+   *    show le menu déroulant, ou hide le menu déroulant
+   */
   onToggleMenuBurgerBtn(){
 
-    /* on change l'etat du menu à chaque clic */
-    this.menuMobileIsOpen = !this.menuMobileIsOpen;
+      if(this.freeBtnBurger){
+        this.freeBtnBurger = false;
+        /* on évite les appuis trop rapide sur le bouton */
+        window.setTimeout( () => {
+          this.freeBtnBurger = true;
+        },400);
+        /* on change l'etat du menu à chaque clic */
+        this.toggleMenuMobileIsOpen();
+        /* lance l'animation du bouton burger*/
+        this.launchAnimationBurgerBtn();
+      }
 
-    if(this.menuMobileIsOpen){
-      this.btnMenuMobileState = "close";
-    }
-    else {
-        this.btnMenuMobileState = "opening";
-        this.btnMenuMobileState = "open";
-    }
 
   }
 
   /* close the menu on mobile when <a> is visited    */
   onToggleLink(){
+    /* on force le menu ouvert */
     this.menuMobileIsOpen = true;
+
     this.btnMenuMobileState = "";
     this.menuMobileIsOpen = false;
+
+  }
+
+  /**
+   * En fonction de la classe de id="burgerBtn" lance l'animation
+   *  de fermeture; ouverture du bouton burge
+   */
+  launchAnimationBurgerBtn(){
+
+
+      if(this.menuMobileIsOpen){
+           /*
+               ouverture du menu => animation croix
+           */
+      this.btnMenuMobileState = "closing";
+       setTimeout( ()=>{
+           this.btnMenuMobileState = "close";
+       },300);
+
+     }
+     else {
+           /*
+               fermture du menu => animation bar
+           */
+       this.btnMenuMobileState = "opening";
+       setTimeout( ()=>{
+           this.btnMenuMobileState = "open";
+       },300);
+
+   }
+
+  }
+
+  /**
+  *   inverse l'etat de menuMobileIsOpen
+  */
+  toggleMenuMobileIsOpen(){
+      this.menuMobileIsOpen = !this.menuMobileIsOpen;
   }
 
 }
--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -7,11 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresentationComponent implements OnInit {
 
-
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
 }
